509096a802ab549243fc1ee4bafb16304e64dca5
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2024-02-29 16:55:55 -0700

    feat: deploy auth

--- client/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ client/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "lostandfoundpetsfd158904": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
