9a1cba72d9fa50545815e3d9123125707ed27a1f
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2024-03-22 16:58:45 -0600

    fix: image upload scrolling

--- client/src/components/ImageUpload/ImageUpload.jsx
+++ client/src/components/ImageUpload/ImageUpload.jsx
@@ -1,12 +1,32 @@
 import React from "react";
-import { useState } from "react";
 import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 import "./ImageUpload.css";
 
 const ImageUpload = ({ images, handleImageChange, handleRemoveImage }) => {
   return (
     <div className="images-container">
-      <label htmlFor="file-upload" className="choose-files-button">
+      <div className="images-wrapper">
+        {images.map((image, index) => (
+          <div key={index} className="image-container">
+            <img
+              src={URL.createObjectURL(image)}
+              className="image-preview"
+              alt={`Preview ${index}`}
+            />
+            <button
+              className="delete-button"
+              onClick={() => handleRemoveImage(index)}
+            >
+              <RemoveCircleIcon />
+            </button>
+          </div>
+        ))}
+      </div>
+      <label
+        htmlFor="file-upload"
+        className="choose-files-button"
+        style={images.length != 0 ? { marginLeft: "auto" } : {}}
+      >
         Select Photo(s)
         <input
           id="file-upload"
@@ -17,21 +37,6 @@ const ImageUpload = ({ images, handleImageChange, handleRemoveImage }) => {
           className="input-hidden"
         />
       </label>
-      {images.map((image, index) => (
-        <div key={index} className="image-container">
-          <img
-            src={URL.createObjectURL(image)}
-            className="image-preview"
-            alt={`Preview ${index}`}
-          />
-          <button
-            className="delete-button"
-            onClick={() => handleRemoveImage(index)}
-          >
-            <RemoveCircleIcon />
-          </button>
-        </div>
-      ))}
     </div>
   );
 };
