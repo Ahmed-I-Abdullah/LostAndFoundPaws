cf6cae16573b78a910c8db0f06c3c9421266f45b
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2024-03-24 21:02:00 -0600

    feat: add arrow back button

---
+++ client/src/components/ArrowBackButton/ArrowBackButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+const ArrowBackButton = ({ onClick }) => {
+  return (
+    <IconButton
+      onClick={onClick}
+      aria-label="back"
+      style={{ backgroundColor: '#D9D9D9', padding: 5 }}
+    >
+      <ArrowBackIcon style={{ color: 'black' }} />
+    </IconButton>
+  );
+};
+
+export default ArrowBackButton;
