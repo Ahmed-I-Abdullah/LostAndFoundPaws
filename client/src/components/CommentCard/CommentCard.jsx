d5bf206e2728fb8968081c6d908983ba3e5b95dd
Ahmed Waly <ahmedw1114@gmail.com>
2024-03-25 18:45:35 -0600

    fix comments view

--- client/src/components/CommentCard/CommentCard.jsx
+++ client/src/components/CommentCard/CommentCard.jsx
@@ -1,29 +1,26 @@
-import { PersonOutline } from '@mui/icons-material';
-import { Avatar, Box, IconButton, Typography, Button, TextField } from '@mui/material';
-import React, { useState } from 'react'
-import DeleteIcon from '@mui/icons-material/Delete';
-import theme from '../../theme/theme';
-import EditIcon from '@mui/icons-material/Edit';
-import ReplyIcon from '@mui/icons-material/Reply';
-import FlagIcon from '@mui/icons-material/Flag';
-import HighlightOffIcon from '@mui/icons-material/HighlightOff';
-import CheckIcon from '@mui/icons-material/Check';
-import Modal from '@mui/material/Modal';
-import './CommentCard.css'
+import { PersonOutline } from "@mui/icons-material";
+import {
+  Avatar,
+  Box,
+  IconButton,
+  Typography,
+  Button,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import theme from "../../theme/theme";
+import EditIcon from "@mui/icons-material/Edit";
+import ReplyIcon from "@mui/icons-material/Reply";
+import FlagIcon from "@mui/icons-material/Flag";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import CheckIcon from "@mui/icons-material/Check";
+import Modal from "@mui/material/Modal";
+import { useMobile } from "../../MobileContext";
+import "./CommentCard.css";
+import ReportPost from "../ReportPopup/ReportPopup";
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
-
-const CommentCard = ( {
+const CommentCard = ({
   owner,
   id, //will be used when connected to backend
   avatar,
@@ -33,14 +30,14 @@ const CommentCard = ( {
   parentCommentId,
   parentCommentUsername,
   parentCommentContent,
-  setReply
+  setReply,
 }) => {
   const formatDate = (dateString) => {
     const date = new Date(dateString);
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    return date.toLocaleDateString('en-US', options);
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return date.toLocaleDateString("en-US", options);
   };
-  
+
   const [expandedComment, setExpandedComment] = useState(false);
   const [editing, setEditing] = useState(false);
   const [editedContent, setEditedContent] = useState(content);
@@ -48,105 +45,143 @@ const CommentCard = ( {
   const [openDelete, setOpenDelete] = React.useState(false);
   const handleOpenDelete = () => setOpenDelete(true);
   const handleCloseDelete = () => setOpenDelete(false);
+  const [isReportModalOpen, setIsReportModalOpen] = useState(false);
+
+  const isMobile = useMobile();
 
   const handleEdit = (e) => {
     setEditedContent(e.target.value);
   };
+
+  const handleReport = (reason, description) => {
+    console.log(
+      "Report submitted with reason: ",
+      reason,
+      " and description: ",
+      description
+    );
+    //TODO: Implement Report Functionality
+  };
+
   return (
-    <Box className="posted-comment" style={{backgroundColor: `${theme.palette.custom.greyBkg.comment.bkg}`}}>
+    <Box
+      className="posted-comment"
+      style={{ backgroundColor: `${theme.palette.custom.greyBkg.comment.bkg}` }}
+    >
       <IconButton>
-        <Avatar className="avatar">
-          <PersonOutline className="avatar" />
+        <Avatar
+          style={{
+            width: isMobile ? "50px" : "100px",
+            height: isMobile ? "50px" : "100px",
+          }}
+        >
+          <PersonOutline />
         </Avatar>
       </IconButton>
       <Box className="comment-info">
         <Box className="comment-topbar">
-          <Typography variant="h5">
-            {username}
-          </Typography>
-          <Typography>
-            {formatDate(createdAt)}
-          </Typography>
+          <Typography variant="h5">{username}</Typography>
+          <Typography>{formatDate(createdAt)}</Typography>
         </Box>
-        <Box className="comment-content" style={{color: `${theme.palette.custom.greyBkg.comment.content}`}}>
-          {parentCommentId && 
-            <Typography color= {`${theme.palette.primary.main}`}>
+        <Box
+          className="comment-content"
+          style={{ color: `${theme.palette.custom.greyBkg.comment.content}` }}
+        >
+          {parentCommentId && (
+            <Typography color={`${theme.palette.primary.main}`}>
               {`@${parentCommentUsername} ${parentCommentContent}`}
             </Typography>
-          }
-          {
-            editing?
-            <TextField 
+          )}
+          {editing ? (
+            <TextField
               id="outlined-multiline-static"
               multiline
               placeholder="Write your comment here"
               maxRows={3}
               sx={{
                 width: "100%",
-                margin: "1rem"
+                margin: "1rem",
               }}
               value={editedContent}
               onChange={handleEdit}
             />
-            :
+          ) : (
             <Typography>
-              {expandedComment ? content : content.length > 150 ? content.slice(0, 150) + '...' : content}
+              {expandedComment
+                ? content
+                : content.length > 150
+                ? content.slice(0, 75) + "..."
+                : content}
               {content.length > 150 && (
-                <Button variant="text" sx={{color: `${theme.palette.text.primary}`}} onClick={() => setExpandedComment(!expandedComment)}>
-                  {expandedComment? 'Show less' : 'Show more'}
+                <Button
+                  variant="text"
+                  sx={{ color: `${theme.palette.text.primary}` }}
+                  onClick={() => setExpandedComment(!expandedComment)}
+                >
+                  {expandedComment ? "Show less" : "Show more"}
                 </Button>
               )}
             </Typography>
-          }
+          )}
         </Box>
         <Box className="comment-actions">
-          {owner?
+          {owner ? (
             <>
-              <Button 
+              <Button
                 variant="text"
-                sx={{color: `${theme.palette.text.primary}`}}
-                onClick={()=>setEditing(!editing)}
+                sx={{ color: `${theme.palette.text.primary}` }}
+                onClick={() => setEditing(!editing)}
               >
-                {
-                  editing?
-                    <>
-                      <HighlightOffIcon />
-                      Cancel
-                    </>
-                  :
-                    <>
-                      <EditIcon />
-                      Edit
-                    </>
-                }
+                {editing ? (
+                  <>
+                    <HighlightOffIcon />
+                    <Typography variant="h9">Cancel</Typography>
+                  </>
+                ) : (
+                  <>
+                    <EditIcon />
+                    <Typography variant="h9">Edit</Typography>
+                  </>
+                )}
               </Button>
-              {editing&&
-                <Button 
+              {editing && (
+                <Button
                   variant="text"
-                  sx={{color: `${theme.palette.text.primary}`}}
+                  sx={{ color: `${theme.palette.text.primary}` }}
                 >
                   <CheckIcon />
-                  Save
+                  <Typography variant="h9">Save</Typography>
                 </Button>
-              }
-              <Button variant="text" sx={{color: `${theme.palette.text.primary}`}} onClick={handleOpenDelete}>
+              )}
+              <Button
+                variant="text"
+                sx={{ color: `${theme.palette.text.primary}` }}
+                onClick={handleOpenDelete}
+              >
                 <DeleteIcon />
-                Delete
+                <Typography variant="h9">Delete</Typography>
               </Button>
             </>
-          :
-          <>
-            <Button variant="text" sx={{color: `${theme.palette.text.primary}`}} onClick={() => setReply(id)}>
-              <ReplyIcon />
-              Reply
-            </Button>
-            <Button variant="text" sx={{color: `${theme.palette.text.primary}`}}>
-              <FlagIcon />
-              Report
-            </Button>
-          </>
-          }
-          
+          ) : (
+            <>
+              <Button
+                variant="text"
+                sx={{ color: `${theme.palette.text.primary}` }}
+                onClick={() => setReply(id)}
+              >
+                <ReplyIcon />
+                <Typography variant="h9">Reply</Typography>
+              </Button>
+              <Button
+                variant="text"
+                sx={{ color: `${theme.palette.text.primary}` }}
+                onClick={() => setIsReportModalOpen(true)}
+              >
+                <FlagIcon />
+                <Typography variant="h9">Report</Typography>
+              </Button>
+            </>
+          )}
         </Box>
       </Box>
       <Modal
@@ -155,18 +190,18 @@ const CommentCard = ( {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box 
+        <Box
           sx={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: '60%',
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "60%",
             backgroundColor: `${theme.palette.custom.primaryBkg}`,
-            borderRadius: '1rem',
+            borderRadius: "1rem",
             boxShadow: 24,
             p: 4,
-            textAlign: 'center'
+            textAlign: "center",
           }}
         >
           <Typography id="modal-modal-title" variant="h6" component="h2">
@@ -174,30 +209,29 @@ const CommentCard = ( {
           </Typography>
           <Box
             sx={{
-              display: 'flex',
-              width: '80%',
-              margin: 'auto',
-              gap: '1rem'
+              display: "flex",
+              width: "80%",
+              margin: "auto",
+              gap: "1rem",
             }}
           >
-            <Button
-              variant='outlined'
-              fullWidth
-              onClick={handleCloseDelete}
-            >
+            <Button variant="outlined" fullWidth onClick={handleCloseDelete}>
               No
             </Button>
-            <Button
-              variant='contained'
-              fullWidth
-            >
+            <Button variant="contained" fullWidth>
               Yes
             </Button>
           </Box>
         </Box>
       </Modal>
+      {isReportModalOpen && (
+        <ReportPost
+          onClose={() => setIsReportModalOpen(false)}
+          onReport={handleReport}
+        />
+      )}
     </Box>
-  )
-}
+  );
+};
 
-export default CommentCard
+export default CommentCard;
