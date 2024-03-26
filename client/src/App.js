586a72393d21818428902dabf23cfc420bd47153
Lauraine Baffot <laury2001@gmail.com>
2024-03-26 02:35:31 -0600

    fix: error importing component

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import HomePageTemp from './pages/HomePage/HomePage';
 import CreatePostForm from './pages/CreatePost/CreatePost';
-import MapView from './pages/MapView/MapView;
+import MapView from './pages/MapView/MapView';
 
 function App() {
   return (
