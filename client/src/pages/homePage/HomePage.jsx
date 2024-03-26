d8cda142ea6b221a65aa1f9510b16fd89795017a
Lauraine Baffot <laury2001@gmail.com>
2024-03-26 02:45:25 -0600

    fix: homepage problem

--- client/src/pages/homePage/HomePage.jsx
+++ client/src/pages/homePage/HomePage.jsx
@@ -1,56 +1,17 @@
-import CoolImage from "../../sharedStyles/CoolImage.png";
-import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faList, faMap } from "@fortawesome/free-solid-svg-icons";
-import { rgba } from "polished";
-import Toggle from "../../components/Toggle/Toggle";
-import theme from "../../theme/theme";
-
-const viewTypeOptions = [
-  {
-    label: "List View",
-    icon: <FontAwesomeIcon icon={faList} />,
-    color: rgba(theme.palette.primary.main, 0.3),
-  },
-  {
-    label: "Map View",
-    icon: <FontAwesomeIcon icon={faMap} />,
-    color: rgba(theme.palette.primary.main, 0.3),
-  },
-];
+import CoolImage from '../../sharedStyles/CoolImage.png';
 
 const HomePageTemp = () => {
-  const navigate = useNavigate();
-  const initialIndex = 0;
 
-  const handleToggle = (index) => {
-    const view = viewTypeOptions[index].label;
-    if (view === "List View") {
-      navigate("/");
-    } else if (view === "Map View") {
-      navigate("/mapView");
-    }
-  };
-
-  return (
+  return(
     <div>
-      <div className="toggle">
-        <Toggle
-          options={viewTypeOptions}
-          onToggleCallback={handleToggle}
-          initialIndex={initialIndex}
-        />
-      </div>
-      <div>TODO Add list/map/sightings view here</div>
       <div>
-        <img
-          src={CoolImage}
-          alt="Logo"
-          style={{ width: "40%", height: "auto" }}
-        />
+        TODO Add list/map/sightings view here
+      </div>
+      <div>
+        <img src={CoolImage} alt="Logo" style={{ width: '40%', height: 'auto' }} />
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default HomePageTemp;
