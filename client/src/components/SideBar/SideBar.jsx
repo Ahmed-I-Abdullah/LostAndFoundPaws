635e88c5bf2ccc7d9d8bd46a242b02618610da14
Lauraine Baffot <laury2001@gmail.com>
2024-03-27 04:01:04 -0600

    feat: adding filters sidebar to viewReportings

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -16,7 +16,7 @@ import CustomDropdown from "../DropDown/DropDown";
 import "./SideBar.css";
 import theme from "../../theme/theme";
 
-const SideBar = ({ selectedView, onClose }) => {
+const SideBar = ({ selectedView, isReporting, onClose }) => {
   const [sortBy, setSortBy] = useState("Newest");
   const [species, setSpecies] = useState({
     dog: false,
@@ -29,6 +29,10 @@ const SideBar = ({ selectedView, onClose }) => {
     unknown: false,
   });
   const [locationAway, setLocationAway] = useState(0);
+  const [reportReason, setReportReason] = useState({
+    inappropriate: false,
+    spam: false,
+  });
   const { isMobile } = useMobile();
 
   //TODO: Add functionality to all filters
@@ -46,7 +50,7 @@ const SideBar = ({ selectedView, onClose }) => {
         </IconButton>
       </Box>
 
-      {!isMobile && (
+      {!isMobile && !isReporting && (
         <Box width={"100%"} sx={{ marginTop: "1rem", marginBottom: "1rem" }}>
           <SearchBar placeholder={"Enter city, neighborhood, address"} />
         </Box>
@@ -72,101 +76,141 @@ const SideBar = ({ selectedView, onClose }) => {
         </div>
       )}
 
-      <div className="divider" />
-      <div>
-        <Typography variant="h6">Species</Typography>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={species.dog}
-              onChange={(e) =>
-                setSpecies({ ...species, dog: e.target.checked })
-              }
-            />
-          }
-          label="Dog"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={species.cat}
-              onChange={(e) =>
-                setSpecies({ ...species, cat: e.target.checked })
-              }
-            />
-          }
-          label="Cat"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={species.other}
-              onChange={(e) =>
-                setSpecies({ ...species, other: e.target.checked })
-              }
-            />
-          }
-          label="Other"
-        />
-      </div>
+      {!isReporting && <div className="divider" />}
+      {!isReporting && (
+        <div>
+          <Typography variant="h6">Species</Typography>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={species.dog}
+                onChange={(e) =>
+                  setSpecies({ ...species, dog: e.target.checked })
+                }
+              />
+            }
+            label="Dog"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={species.cat}
+                onChange={(e) =>
+                  setSpecies({ ...species, cat: e.target.checked })
+                }
+              />
+            }
+            label="Cat"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={species.other}
+                onChange={(e) =>
+                  setSpecies({ ...species, other: e.target.checked })
+                }
+              />
+            }
+            label="Other"
+          />
+        </div>
+      )}
 
-      <div className="divider" />
-      <div>
-        <Typography variant="h6">Gender</Typography>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={gender.male}
-              onChange={(e) => setGender({ ...gender, male: e.target.checked })}
-            />
-          }
-          label="Male"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={gender.female}
-              onChange={(e) =>
-                setGender({ ...gender, female: e.target.checked })
-              }
-            />
-          }
-          label="Female"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={gender.unknown}
-              onChange={(e) =>
-                setGender({ ...gender, unknown: e.target.checked })
-              }
-            />
-          }
-          label="Unknown"
-        />
-      </div>
+      {!isReporting && <div className="divider" />}
+      {!isReporting && (
+        <div>
+          <Typography variant="h6">Gender</Typography>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={gender.male}
+                onChange={(e) =>
+                  setGender({ ...gender, male: e.target.checked })
+                }
+              />
+            }
+            label="Male"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={gender.female}
+                onChange={(e) =>
+                  setGender({ ...gender, female: e.target.checked })
+                }
+              />
+            }
+            label="Female"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={gender.unknown}
+                onChange={(e) =>
+                  setGender({ ...gender, unknown: e.target.checked })
+                }
+              />
+            }
+            label="Unknown"
+          />
+        </div>
+      )}
 
-      <div className="divider" />
-      <div>
-        <Typography variant="h6">Location Away</Typography>
-        <Slider
-          value={locationAway}
-          onChange={(e, value) => setLocationAway(value)}
-          min={0}
-          max={30}
-          step={1}
-          marks={[
-            { value: 0, label: "0 km" },
-            { value: 30, label: "30 km" },
-          ]}
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            width: "90%",
-            margin: "auto",
-          }}
-        />
-      </div>
+      {isReporting && <div className="divider" />}
+      {isReporting && (
+        <div>
+          <Typography variant="h6">Report Reason</Typography>
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={reportReason.inappropriate}
+                onChange={(e) =>
+                  setReportReason({
+                    ...reportReason,
+                    inappropriate: e.target.checked,
+                  })
+                }
+              />
+            }
+            label="Inappropriate"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={reportReason.spam}
+                onChange={(e) =>
+                  setReportReason({ ...reportReason, spam: e.target.checked })
+                }
+              />
+            }
+            label="Spam"
+          />
+        </div>
+      )}
+
+      {!isReporting && <div className="divider" />}
+      {!isReporting && (
+        <div>
+          <Typography variant="h6">Location Away</Typography>
+          <Slider
+            value={locationAway}
+            onChange={(e, value) => setLocationAway(value)}
+            min={0}
+            max={30}
+            step={1}
+            marks={[
+              { value: 0, label: "0 km" },
+              { value: 30, label: "30 km" },
+            ]}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              width: "90%",
+              margin: "auto",
+            }}
+          />
+        </div>
+      )}
 
       <div
         style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
