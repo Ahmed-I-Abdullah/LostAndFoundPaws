0886df429c2f5e55818a24f928911835222c7877
Ahmed Abdullah <ahmedi3aaa@hotmail.com>
2024-03-27 18:35:48 -0600

    feat: login form validation

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
-import { useMobile } from '../../MobileContext';
+import React from "react";
+import { useMobile } from "../../MobileContext";
 import { Link } from "react-router-dom";
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 import "../../sharedStyles/SharedStyles.css";
-import PawLogo from '../../sharedStyles/PawLogo.png';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import PawLogo from "../../sharedStyles/PawLogo.png";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import * as Yup from "yup";
+import { Formik, Form } from "formik";
 
 const Login = () => {
   const { isMobile } = useMobile();
 
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().required("Email or username is required"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
-    <div className={`${isMobile ? 'account-wrapper-mobile' : 'account-wrapper'}`}>
-      <div className={`${isMobile ? 'account-container-mobile' : 'account-container'}`}>
+    <div
+      className={`${isMobile ? "account-wrapper-mobile" : "account-wrapper"}`}
+    >
+      <div
+        className={`${
+          isMobile ? "account-container-mobile" : "account-container"
+        }`}
+      >
         <div className="close-button">
           <IconButton href="./" aria-label="close">
             <CloseIcon />
@@ -27,22 +49,58 @@ const Login = () => {
           <h1>Log In</h1>
           <div className="divider"></div>
         </div>
-        <form>
-          <div className="account-form-component">
-            <TextField label="Email or Username" variant="outlined" fullWidth />
-          </div>
-          <div className="account-form-component">
-            <TextField label="Password" variant="outlined" type="password" fullWidth />
-            <Link to="/forgetPassword" className="forgot-password-link">Forgot Password?</Link>
-          </div>
-          <div className="account-form-component">
-            <Button type="submit" variant="contained" color="primary">
-              Log In
-            </Button>
-          </div>
-        </form>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ errors, touched, handleSubmit, setFieldValue, values }) => (
+            <Form onSubmit={handleSubmit} style={{ height: "100%" }}>
+              <div className="account-form-component">
+                <TextField
+                  label="Email or Username"
+                  variant="outlined"
+                  error={errors.email && touched.email}
+                  helperText={touched.email ? errors.email : ""}
+                  value={values.email}
+                  onChange={(event) => {
+                    setFieldValue("email", event.target.value);
+                  }}
+                  fullWidth
+                />
+              </div>
+              <div className="account-form-component">
+                <TextField
+                  label="Password"
+                  variant="outlined"
+                  type="password"
+                  error={errors.password && touched.password}
+                  helperText={touched.password ? errors.password : ""}
+                  value={values.password}
+                  onChange={(event) => {
+                    setFieldValue("password", event.target.value);
+                  }}
+                  fullWidth
+                />
+                <Link to="/forgetPassword" className="forgot-password-link">
+                  Forgot Password?
+                </Link>
+              </div>
+              <div className="account-form-component">
+                <Button type="submit" variant="contained" color="primary">
+                  Log In
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
         <div className="account-link-container">
-          <span>Don't have an account? <Link to="/signup" className="account-link">Sign up</Link></span>
+          <span>
+            Don't have an account?{" "}
+            <Link to="/signup" className="account-link">
+              Sign up
+            </Link>
+          </span>
         </div>
       </div>
     </div>
