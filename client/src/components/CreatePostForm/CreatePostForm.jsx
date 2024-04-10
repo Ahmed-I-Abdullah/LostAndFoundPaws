e3d195e0084bec1c42c7fa36d9e9e9c82e9b098b
DrCoconuttt <richardharder100@gmail.com>
2024-04-10 01:20:48 -0600

    fix: name unknown for found pets, not lost pets

--- client/src/components/CreatePostForm/CreatePostForm.jsx
+++ client/src/components/CreatePostForm/CreatePostForm.jsx
@@ -47,7 +47,7 @@ const CreatePostForm = ({ isEdit, postData, handleSubmit, isSubmitting  }) => {
   const { userState, currentUser } = useUser();
   const [initialValues, setInitialValues] = useState({
     type: isEdit ? postData.status : "Lost",
-    name: isEdit ? postData.name : "Unknown",
+    name: isEdit ? postData.name : "",
     gender: isEdit ? postData.gender : "",
     summary: isEdit ? postData.summary : "",
     description: isEdit ? postData.description : "",
@@ -139,10 +139,10 @@ const CreatePostForm = ({ isEdit, postData, handleSubmit, isSubmitting  }) => {
                         options={postTypeOptions}
                         onToggleCallback={(index) => {
                           setFieldValue("type", postTypeOptions[index].label)
-                          if(index == 0 && !values.name) {
-                            setFieldValue("name", "Unknown")
-                          } else if(index == 1 && values.name == "Unknown") {
+                          if(index == 0 && values.name == "" || values.name == "Unknown") {
                             setFieldValue("name", "")
+                          } else if(index == 1 && values.name == "" || values.name == "Unknown") {
+                            setFieldValue("name", "Unknown")
                           }
                         }
                         }
